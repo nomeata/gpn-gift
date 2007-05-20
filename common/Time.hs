955e1227c79511123b1e0d7671e715a5a7c105ff
Joachim Breitner <mail@joachim-breitner.de>
2007-05-20 22:09:02 +0000

    Read/Show für Time/RunTime

--- common/Time.hs
+++ common/Time.hs
@@ -7,10 +7,19 @@ data Time = Time { tDay :: Int, tHour :: Int, tMin :: Int } deriving (Eq, Ord)
 data RunTime = RunTime { rtHour :: Int, rtMin :: Int } deriving (Eq, Ord)
 
 instance Show Time where
-	show time = printf "Tag %d − %02.0d:%02.0d" (tDay time) (tHour time) (tMin time)
-instance Show RunTime where
-	show time = printf "%02.0dh%02.0d" (rtHour time) (rtMin time)
+	show time = show (tDay time, tHour time, tMin time)
+instance Read Time where
+	readsPrec d str = [(Time day hour min,out)]
+	  where ((day,hour,min),out):_ = readsPrec d str 
 
+instance Show RunTime where
+	show time = show (rtHour time, rtMin time)
+instance Read RunTime where
+	readsPrec d str = [(RunTime hour min,out)]
+	  where ((hour,min),out):_ = readsPrec d str 
+
+printTime  time = printf "Tag %d − %02.0d:%02.0d" (tDay time) (tHour time) (tMin time)
+printRunTime time = printf "%02.0dh%02.0d" (rtHour time) (rtMin time)
 
 now = do
 	time <- getClockTime >>= toCalendarTime
