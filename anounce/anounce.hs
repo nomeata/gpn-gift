a21bdbeba9552460ba6262e97357d2ec19a87b54
Joachim Breitner <mail@joachim-breitner.de>
2007-05-20 22:50:10 +0000

    Logo im Hintergrund, Farben passend

--- anounce/anounce.hs
+++ anounce/anounce.hs
@@ -33,28 +33,39 @@ render canvas = do
 	time <- now
 	win <- widgetGetDrawWindow canvas
 	--(width, height) <- widgetGetSize canvas
-	renderWithDrawable win $ renderC time test_data
+	withImageSurfaceFromPNG "GPN6_logo.png" $ \logo -> 
+		renderWithDrawable win $ renderC logo time test_data
 
-renderC now events = do
+renderC logo now events = do
 	selectFontFace "Mono" FontSlantNormal FontWeightNormal
-	drawbg
+	drawbg logo
 	clock now
 	fahrplan now events
 	return True
 
-drawbg = do
-	setSourceRGB 1 1 1
+drawbg logo = do
+	save
+	setSourceRGB 0.2 0.2 0.2
 	paint
+	w <- imageSurfaceGetWidth logo
+	h <- imageSurfaceGetHeight logo
+	setSourceSurface logo
+		(fromIntegral ((width - w) `div` 2))
+		(fromIntegral  (height - h - 20))
+	paint
+	restore
 
 clock time = do
+	save
 	let pad = 20
 	let text = printTime time
 	setFontSize 20
-	setSourceRGB 0 0 0
+	setSourceRGB 1 1 1
   	(TextExtents xb yb w h _ _) <- textExtents text
 	showText (show xb)
 	moveTo (width - w - pad) (h + pad)
 	showText text
+	restore
 
 fahrplan now events = do
 	let show_events = take 10 $ filter (not . isPassed) $ map (label now) $ events
@@ -68,19 +79,21 @@ label time event | eEndTime event < time  = (Passed,    event)
                  | otherwise              = (Future,    event)
 
 markup (lable, event) line = do
+	save
 	let place_and_time = printf "[%s @ %s]"
 		(show (eTime event)) (eRoom event)
 	let y = 50 * line
 	moveTo 20 y
 	setFontSize 20
-	setSourceRGB 0.3 0.3 0.3
+	setSourceRGB 0.8 0.8 0.8
 	showText place_and_time
 	setFontSize 40
 	moveTo 350 y
 	case lable of
 		Running -> setSourceRGB 1 0 0 
-		Future  -> setSourceRGB 0 0 0
+		Future  -> setSourceRGB 1 1 1
 	showText (eName event)
+	restore
 
 eName    (_, s, _, _, _) = s
 eRoom    (_, _, s, _, _) = s
@@ -100,7 +113,7 @@ test_data = [
 	(1,
 	"Gulaschgh",
 	"Chaos",
-	Time 0 22 23,
+	Time 1 00 23,
 	RunTime 1 15
 	),
 	(2,
