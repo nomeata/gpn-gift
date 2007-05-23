c26f638755235bc566d0cb78a882100ef1e836a5
Joachim Breitner <mail@joachim-breitner.de>
2007-05-23 18:18:36 +0000

    ByteString für FileRef (wegen strikter evaluierung)

--- common/FileRef.hs
+++ common/FileRef.hs
@@ -2,14 +2,15 @@ module FileRef (FileRef, newFileRef, readFileRef, writeFileRef) where
 
 import System.FilePath
 import Data.IORef
+import qualified Data.ByteString.Char8 as BS -- Faster and Strict
 
 data (Show a, Read a) => FileRef a = FR FilePath (IORef a)
 
 newFileRef :: (Show a, Read a) => FilePath -> IO (FileRef a)
 newFileRef path = do
 	absPath <- makeRelativeToCurrentDirectory path
-	content <- readFile absPath
-	ref <- newIORef (read content)
+	content <- BS.readFile absPath
+	ref <- newIORef (read (BS.unpack content))
 	return $ FR absPath ref
 
 readFileRef :: (Show a, Read a) => FileRef a -> IO a
@@ -18,4 +19,4 @@ readFileRef (FR _ ref) = readIORef ref
 writeFileRef :: (Show a, Read a) => FileRef a -> a -> IO ()
 writeFileRef (FR path ref) v = do
 	writeIORef ref v
-	writeFile path (show v)
+	BS.writeFile path (BS.pack (show v))
