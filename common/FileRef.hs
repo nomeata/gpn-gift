6599650f63bc36d6980becb13dcd3de1a7d7b046
Joachim Breitner <mail@joachim-breitner.de>
2007-05-28 19:21:00 +0000

    Doku fuer FileRef.hs

--- common/FileRef.hs
+++ common/FileRef.hs
@@ -1,11 +1,15 @@
+-- | The FileRef object is similar to a 'IORef' with the difference that it reads it's initial data from a given file and writes, upon modification, the new data to that file.
 module FileRef (FileRef, newFileRef, readFileRef, writeFileRef) where
 
 import System.FilePath
 import Data.IORef
 import qualified Data.ByteString.Char8 as BS -- Faster and Strict
 
+
+-- | The FileRef data type is opaque and can store only data that can be written and read using 'Show' and 'Read'.
 data (Show a, Read a) => FileRef a = FR FilePath (IORef a)
 
+-- | Creates a new 'FileRef' and reads the data from the given file
 newFileRef :: (Show a, Read a) => FilePath -> IO (FileRef a)
 newFileRef path = do
 	absPath <- makeRelativeToCurrentDirectory path
@@ -13,9 +17,11 @@ newFileRef path = do
 	ref <- newIORef (read (BS.unpack content))
 	return $ FR absPath ref
 
+-- | Returns the data stored in the 'FileRef'
 readFileRef :: (Show a, Read a) => FileRef a -> IO a
 readFileRef (FR _ ref) = readIORef ref
 
+-- | Stores new data in the 'FileRef', updating the file.
 writeFileRef :: (Show a, Read a) => FileRef a -> a -> IO ()
 writeFileRef (FR path ref) v = do
 	writeIORef ref v
