b92046a3a989a7bce8255a1de6514e8479d00753
Joachim Breitner <mail@joachim-breitner.de>
2007-05-29 19:13:05 +0000

    Client Anfang, noch ohne Funktionalität

--- client/client.hs
+++ client/client.hs
@@ -1,13 +1,36 @@
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Glade
-import CfgPars
+import Network
+import Control.Monad
+import IO
+import Data.Maybe
+
+import Time
+import DatT
 
 main :: IO()
 main = do
-	g <- readFile "data/client.cnf"
+	conf <- read `liftM` readFile "data/client.cnf" -- reading the server.cnf
+	let port = read (fromJust $ lookup "port" conf) :: Int
+	h <- connectTo "localhost"  (PortNumber (fromIntegral port))
+	hSetBuffering h LineBuffering
+	login h conf
+
 	initGUI
+
         Just xml <- xmlNew "client.glade"
         window <- xmlGetWidget xml castToWindow "window1"
-	windowSetTitle window (parser "name" g ++  " - G*I*F*T Client")
+
+	tv <- xmlGetWidget xml castToTreeView "treeview1"
+	b_quit <- xmlGetWidget xml castToButton "quit"
+	b_refresh <- xmlGetWidget xml castToButton "refresh"
+
+
+	onClicked b_quit $ do widgetDestroy window
+
 	onDestroy window mainQuit
 	mainGUI
+
+login h conf = do
+	hPutStrLn h "guest"
+	hPutStrLn h "guest"
