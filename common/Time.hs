9f3d5162f321fffc42e4426b7b15a46b1a35129f
Joachim Breitner <mail@joachim-breitner.de>
2007-05-30 10:27:41 +0000

    Time Datentyp abstrakt gemacht und dokumentiert

--- common/Time.hs
+++ common/Time.hs
@@ -1,9 +1,11 @@
-module Time where
+module Time (Time, RunTime, printTime, printRunTime, now, addRunTime) where
 
 import System.Time
 import Text.Printf
 
+-- | Data types for Event Times 
 data Time = Time { tDay :: Int, tHour :: Int, tMin :: Int } deriving (Eq, Ord)
+-- | Data types for Event Run Times
 data RunTime = RunTime { rtHour :: Int, rtMin :: Int } deriving (Eq, Ord)
 
 instance Show Time where
@@ -18,16 +20,21 @@ instance Read RunTime where
 	readsPrec d str = [(RunTime hour min,out)]
 	  where ((hour,min),out):_ = readsPrec d str 
 
+-- | Pretty Printer
 printTime :: Time -> String
 printTime  time = printf "Tag %d − %02.0d:%02.0d" (tDay time) (tHour time) (tMin time)
+-- | Pretty Printer
 printRunTime :: RunTime -> String
 printRunTime time = printf "%02.0dh%02.0d" (rtHour time) (rtMin time)
 
+-- | Return the current time
 now :: IO (Time)
 now = do
 	time <- getClockTime >>= toCalendarTime
 	return $ Time { tDay = ctDay time - 00, tHour = ctHour time, tMin = ctMin time}
 
+-- | Given a start time and a run time, retun the end time
+addRunTime :: Time -> RunTime -> Time
 addRunTime start rt= fix $ sum 
   where sum = start {tHour = tHour start + rtHour rt, tMin = tMin start + rtMin rt}
   	fix = fixd . fixh
